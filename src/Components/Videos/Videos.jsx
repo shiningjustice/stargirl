e08cd1853cb2ac513b9e87522e2c8b7b146c836e
Phoebe Law <phoebebasilio@gmail.com>
2020-02-11 20:32:37 -0800

    adds code to generate videos from dataStore

--- src/Components/Videos/Videos.jsx
+++ src/Components/Videos/Videos.jsx
@@ -1,10 +1,32 @@
 import React from 'react';
+import dataStore from '../../helpers/dataStore';
 import './Videos.css';
 
 function Videos() {
+
+  const formatVideoHtml = () => {
+    const videos = dataStore.videos;
+  
+    return videos.map((video, index) => 
+      <div key={index}>
+        <h2 className='header sup'>{video.title}</h2>
+        <iframe 
+        title={index}
+        width="560" 
+        height="315" 
+        src={video.src}
+        frameBorder="0" 
+        allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" 
+        allowFullScreen
+        ></iframe>
+      </div>
+    )
+  }
+
   return (
     <div className="Videos">
-
+      <h1 className='header'>Videos</h1>
+      {formatVideoHtml()}
     </div>
   )
 }
---
+++ src/helpers/dataStore.js
@@ -0,0 +1,18 @@
+const dataStore = {
+  videos: [
+    {
+      title: "Happy Birthday Hannah (2010)", 
+      src: "https://www.youtube.com/embed/tncd3K2qsfQ" //embedded src
+    }, 
+    {
+      title: "Happy Birthday Hannah (2011)",
+      src: "https://youtube.com/embed/snnC-LT8sn4"
+    },
+    {
+      title: "Happy Birthday Hannah (2014)",
+      src: "https://www.youtube.com/embed/GWjedyCMm3Y"
+    }
+  ]
+}
+
+export default dataStore;
