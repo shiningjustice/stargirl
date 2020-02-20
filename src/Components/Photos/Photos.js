3004d9051eeeb7a45f5df27d954d2c122bacbb6d
Phoebe Law <phoebebasilio@gmail.com>
2020-02-20 13:32:24 -0800

    adds photos component and grid styling

--- src/Components/Photos/Photos.js
+++ src/Components/Photos/Photos.js
@@ -1,10 +1,21 @@
 import React from 'react';
+import dataStore from '../../helpers/dataStore';
 import './Photos.css';
 
 function Photos() {
+  const formatPhotoHtml = () => {
+    const photos = dataStore.photos;
+
+    return photos.map((photo, index) => (
+      <img className="Photo" key={index} src={photo.src} />
+    ))
+  }
   return (
     <div className="Photos">
-
+      <h1 className="header">Photos</h1>
+      <div className="PhotosWrapper">
+        {formatPhotoHtml()}
+      </div>
     </div>
   )
 }
