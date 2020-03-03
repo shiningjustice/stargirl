12fc182996df82ff06e5bc231b2831417c0cdbb9
Phoebe Law <phoebebasilio@gmail.com>
2020-03-02 16:52:23 -0800

    adds app endpoint and token

---
+++ src/config.js
@@ -0,0 +1,10 @@
+export default {
+  REACT_APP_API_ENDPOINT: 
+    process.env.NODE_ENV === 'production'
+      ? process.env.REACT_APP_API_ENDPOINT
+      : 'http://localhost:8000/api',
+  TOKEN_KEY: 
+    process.env.NODE_ENV === 'production'
+      ? process.env.REACT_APP_API_TOKEN
+      : 'stargirlAuthToken'
+}
